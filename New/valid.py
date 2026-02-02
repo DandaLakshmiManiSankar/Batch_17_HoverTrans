59db2eb29a863dd80b46ecdf42ad84eacdb8bfb4
Lakshmi Mani Sankar Danda <171979917+DandaLakshmiManiSankar@users.noreply.github.com>
2026-02-02 11:38:35 +0530

    Enhance validation function with TTA support
    Added support for test-time augmentation (TTA) in the validation function. Improved code readability and fixed minor typos.

--- New/valid.py
+++ New/valid.py
@@ -3,44 +3,57 @@ from utils import confusion_matrix
 from sklearn.metrics import roc_auc_score
 
 
-def valid(config, net, val_loader, criterion):                                       
+def valid(config, net, val_loader, criterion, use_tta=False):
     device = next(net.parameters()).device
     net.eval()
 
-    print("START VALIDING")
+    print("START VALIDATING")
     epoch_loss = 0
     y_true, y_score = [], []
 
-    cm = torch.zeros((config.class_num, config.class_num))
-    for i, pack in enumerate(val_loader):
-        images = pack['imgs'].to(device)
-        if images.shape[1] == 1:
-            images = images.expand((-1, 3, -1, -1))
-        names = pack['names']
-        labels = pack['labels'].to(device)
+    cm = torch.zeros((config.class_num, config.class_num), device=device)
 
-        output = net(images)
+    with torch.no_grad():
+        for pack in val_loader:
+            images = pack['imgs'].to(device)
+            labels = pack['labels'].to(device)
 
-        loss = criterion(output, labels)
+            if images.shape[1] == 1:
+                images = images.expand((-1, 3, -1, -1))
 
-        pred = output.argmax(dim=1)
-        y_true.append(labels.detach().item())
-        y_score.append(output[0].softmax(0)[1].item())
+            # ===============================
+            # TTA: Original + Horizontal Flip
+            # ===============================
+            if use_tta:
+                output1 = net(images)
+                images_flip = torch.flip(images, dims=[3])
+                output2 = net(images_flip)
+                output = (output1 + output2) / 2
+            else:
+                output = net(images)
 
-        cm = confusion_matrix(pred.detach(), labels.detach(), cm)
-        epoch_loss += loss.cpu()
-            
+            loss = criterion(output, labels)
+            epoch_loss += loss.item()
+
+            probs = torch.softmax(output, dim=1)
+            preds = probs.argmax(dim=1)
+
+            cm = confusion_matrix(preds, labels, cm)
+
+            y_true.extend(labels.cpu().numpy().tolist())
+            y_score.extend(probs[:, 1].cpu().numpy().tolist())
+
     avg_epoch_loss = epoch_loss / len(val_loader)
 
-    #   t  N  P
-    # p
-    # N    TN FN
-    # P    FP TP
+    # ===============================
+    # Metrics
+    # ===============================
     acc = cm.diag().sum() / cm.sum()
     spe, sen = cm.diag() / (cm.sum(dim=0) + 1e-6)
     pre = cm.diag()[1] / (cm.sum(dim=1) + 1e-6)[1]
     rec = sen
-    f1score = 2*pre*rec / (pre+rec+ 1e-6)
+    f1score = 2 * pre * rec / (pre + rec + 1e-6)
+
     auc = roc_auc_score(y_true, y_score)
-    
-    return [avg_epoch_loss, acc, sen, spe, auc, pre, f1score]
+
+    return avg_epoch_loss, acc, sen, spe, auc, pre, f1score
